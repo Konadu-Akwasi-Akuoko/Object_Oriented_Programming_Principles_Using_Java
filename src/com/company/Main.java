b55655b885bdda91777ab60e428d79b908f0dd44
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2021-08-01 04:19:50 +0000

    Class variable and Instance variables.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
         MyClass myObject = new MyClass();
         myObject.ReadName();
+        System.out.println("This is a class variable - " + MyClass.Gender);
+        System.out.println("This is an instance variable - " + myObject.FirstName);
     }
 }
 
--- src/com/company/MyClass.java
+++ src/com/company/MyClass.java
@@ -1,9 +1,10 @@
 package com.company;
 
 public class MyClass {
-    String FirstName = "Akwasi";
+    String FirstName = "Akwasi"; //Instance variable.
     String MiddleName = "Konadu";
     String LastName = "Akuoko";
+    public static String Gender= "I am a boy"; //Class variable.
 
     public void ReadName() {
         System.out.println("My name is " + FirstName + " " + MiddleName + " " + LastName);
