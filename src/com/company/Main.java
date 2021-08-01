d4b088a090a78040a048445d06ae37c25a034d11
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2021-08-01 04:33:21 +0000

    Attributes.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,11 @@ public class Main {
     public static void main(String[] args) {
         MyClass myObject = new MyClass();
         myObject.ReadName();
-        System.out.println("This is a class variable - " + MyClass.Gender);
-        System.out.println("This is an instance variable - " + myObject.FirstName);
+        System.out.println("This is a class variable - " + MyClass.Gender); //Accessing a class variable (attribute).
+        System.out.println("This is an instance variable - " + myObject.FirstName); //Accessing an instance variable (attribute).
+
     }
 }
+
+//Learn more about OOP here: https://www.w3schools.com/java/java_oop.asp
 
--- src/com/company/MyClass.java
+++ src/com/company/MyClass.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class MyClass {
     String FirstName = "Akwasi"; //Instance variable.
-    String MiddleName = "Konadu";
+    String MiddleName = "Konadu"; //Attribute - variables in a class are called attributes of the class.
     String LastName = "Akuoko";
     public static String Gender= "I am a boy"; //Class variable.
 
