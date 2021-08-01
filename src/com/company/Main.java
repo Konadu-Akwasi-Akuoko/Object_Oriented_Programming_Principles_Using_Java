3c62df670dabd43405cec89b403b9af56973e263
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2021-08-01 16:03:34 +0000

    The use of Constructors.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,7 +9,12 @@ public class Main {
         System.out.println("This is an instance variable - " + myObject.FirstName); //Accessing an instance variable (attribute).
         myObject.NonStaticMethod(); //Accessing a non-static method/function through the object.
         MyClass.StaticMethod(); //Accessing a static method/function through the object.
-
+        //Creating an object from a constructor and displaying the results.
+        MyClass myConstructorObject = new MyClass("Yaw", "Vivian", "Ama", "Kwabena");
+        System.out.println("My father's name is: " + myConstructorObject.FatherName);
+        System.out.println("My mother's name is: " + myConstructorObject.MotherName);
+        System.out.println("My sister's name is: " + myConstructorObject.SisterName);
+        System.out.println("My brother's name is: " + myConstructorObject.BrotherName);
     }
 }
 
--- src/com/company/MyClass.java
+++ src/com/company/MyClass.java
@@ -5,6 +5,24 @@ public class MyClass {
     String MiddleName = "Konadu"; //Attribute - variables in a class are called attributes of the class.
     String LastName = "Akuoko";
     public static String Gender = "I am a boy"; //Class variable.
+    //The following attributes will be initialized by the constructor.
+    String FatherName;
+    String MotherName;
+    String SisterName;
+    String BrotherName;
+
+    public MyClass() {  //This is a constructor, and it is used to initialize variables.
+
+    }
+
+    //Constructors can have two or more methods, thus they support method overloading, this means that you can
+    //set many constructors as you want but all with different parameters.
+    public MyClass(String fatherName, String motherName, String sisterName, String brotherName) {
+        this.FatherName = fatherName;
+        this.MotherName = motherName;
+        this.SisterName = sisterName;
+        this.BrotherName = brotherName;
+    }
 
     public void ReadName() { //Class method or function.
         System.out.println("My name is " + FirstName + " " + MiddleName + " " + LastName);
@@ -15,7 +33,7 @@ public class MyClass {
                 " and referencing them through the dot syntax(.)");
     }
 
-    public static void StaticMethod(){
+    public static void StaticMethod() {
         System.out.println("This is a static method and can be called/accessed without creating any object," +
                 " but through class referencing and the dot syntax(.)");
     }
