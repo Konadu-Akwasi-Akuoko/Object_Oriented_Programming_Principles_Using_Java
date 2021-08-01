880cc8aecf05c71c9cf4220df1192eca4feec3e1
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2021-08-01 04:12:03 +0000

    Classes and Objects

--- src/com/company/MyClass.java
+++ src/com/company/MyClass.java
@@ -1,4 +1,11 @@
 package com.company;
 
 public class MyClass {
+    String FirstName = "Akwasi";
+    String MiddleName = "Konadu";
+    String LastName = "Akuoko";
+
+    public void ReadName() {
+        System.out.println("My name is " + FirstName + " " + MiddleName + " " + LastName);
+    }
 }
